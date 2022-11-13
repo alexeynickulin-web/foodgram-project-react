e66df8246e30a27742d0e3b03c0f1124702e5d54
Алексей Никулин <alexeynickulin@yandex.ru>
2022-11-14 00:24:46 +0300

    corrects users

--- backend/api/serializers/users.py
+++ backend/api/serializers/users.py
@@ -52,7 +52,7 @@ class CustomUserCreateSerializer(UserSerializer):
             email=validated_data['email'],
             username=validated_data['username'],
             first_name=validated_data['first_name'],
-            last_name=validated_data['first_name']
+            last_name=validated_data['last_name']
         )
         user.set_password(validated_data['password'])
         user.save()
